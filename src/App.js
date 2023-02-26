bbcfc3741c906daa53ad77c9d7e36bd4f3484fab
Skylark96 <skylark.b96@gmail.com>
2023-02-26 14:44:36 +0100

    final results

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ export default class App extends Component {
     bio: ": { Lazy But Creative  } :",
     imgSrc: <img id='img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIWqGKMsyBO5KeG30nMTAxfJIBqanq-51whw&usqp=CAU" alt="curly hair man illustration" />,
     profession: "Front end web devoloper",
-    show: true
+    show: true,
+    count:0
   }
   click=()=>this.setState({show:!this.state.show})
+  time=()=>this.setState({count:this.state.count+1})
+
+  componentDidMount(){
+    setInterval(this.time, 1000)
+  }
   render() {
     return (
       <div className="all">
+        <div className="count">{this.state.count}</div>
         <button className="btm" onClick={this.click}>Show profile</button>{
           this.state.show&&
       <div className="mainBox">
